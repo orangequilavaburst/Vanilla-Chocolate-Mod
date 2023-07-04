d79732c374fda72a66add656550f45e10e239c41
J8-Bit <orangequilavaburst@gmail.com>
2023-07-03 23:17:57 -0400

    vague work on chocolate rain bow

--- src/main/java/xyz/j8bit_forager/nillachoco/item/ChocolateRainBowItem.java
+++ src/main/java/xyz/j8bit_forager/nillachoco/item/ChocolateRainBowItem.java
@@ -1,7 +1,24 @@
 package xyz.j8bit_forager.nillachoco.item;
 
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ProjectileWeaponItem;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.function.Predicate;
 
@@ -15,11 +32,104 @@ public class ChocolateRainBowItem extends ProjectileWeaponItem {
 
     @Override
     public Predicate<ItemStack> getAllSupportedProjectiles() {
-        return null;
+        return ARROW_ONLY;
     }
 
     @Override
     public int getDefaultProjectileRange() {
         return 1;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 72000;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        boolean flag = !pPlayer.getProjectile(itemstack).isEmpty();
+
+        InteractionResultHolder<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, pLevel, pPlayer, pHand, flag);
+        if (ret != null) return ret;
+
+        Player player = pPlayer;
+        Vec3 view = player.getViewVector(0);
+        Vec3 eyeVec = player.getEyePosition(0);
+        double distance = 50.0d;
+        BlockHitResult ray = pLevel.clip(new ClipContext(eyeVec, eyeVec.add(
+                view.x * distance, view.y * distance, view.z * distance),
+                ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
+        Vec3 hit = ray.getLocation();
+
+        if (ray.distanceTo(player) < 50.0f && ray != BlockHitResult.miss(ray.getLocation(), ray.getDirection(), ray.getBlockPos())){
+
+            pLevel.addParticle(ParticleTypes.EFFECT, hit.x, hit.y, hit.z, 0.0f, 0.0f, 0.0f);
+
+        }
+
+        if (!pPlayer.getAbilities().instabuild && !flag) {
+            return InteractionResultHolder.fail(itemstack);
+        } else {
+            pPlayer.startUsingItem(pHand);
+            return InteractionResultHolder.consume(itemstack);
+        }
+    }
+
+    @Override
+    public void releaseUsing(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving, int pTimeLeft) {
+        if (pEntityLiving instanceof Player player) {
+            boolean flag = player.getAbilities().instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, pStack) > 0;
+            ItemStack itemstack = player.getProjectile(pStack);
+
+            int i = this.getUseDuration(pStack) - pTimeLeft;
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(pStack, pLevel, player, i, !itemstack.isEmpty() || flag);
+            if (i < 0) return;
+
+            Vec3 view = player.getViewVector(0);
+            Vec3 eyeVec = player.getEyePosition(0);
+            double distance = i / 20.0d;
+            BlockHitResult ray = pLevel.clip(new ClipContext(eyeVec, eyeVec.add(
+                    view.x * distance, view.y * distance, view.z * distance),
+                    ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player));
+            Vec3 hit = ray.getLocation();
+
+            if (ray.distanceTo(player) < 50.0f && ray != BlockHitResult.miss(ray.getLocation(), ray.getDirection(), ray.getBlockPos())){
+
+                pLevel.addParticle(ParticleTypes.EFFECT, hit.x, hit.y, hit.z, 0.0f, 0.0f, 0.0f);
+
+            }
+
+            if (!itemstack.isEmpty() || flag) {
+                if (itemstack.isEmpty()) {
+                    itemstack = new ItemStack(Items.ARROW);
+                }
+
+                float f = 1.0f;
+                if (!((double)f < 0.1D)) {
+                    boolean flag1 = player.getAbilities().instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem)itemstack.getItem()).isInfinite(itemstack, pStack, player));
+                    if (!pLevel.isClientSide) {
+                        ArrowItem arrowitem = (ArrowItem)(itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
+
+                        // arrow stuff
+
+                        pStack.hurtAndBreak(1, player, (p_289501_) -> {
+                            p_289501_.broadcastBreakEvent(player.getUsedItemHand());
+                        });
+
+                    }
+
+                    pLevel.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F / (pLevel.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!flag1 && !player.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                        if (itemstack.isEmpty()) {
+                            player.getInventory().removeItem(itemstack);
+                        }
+                    }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
+                }
+            }
+        }
     }
 }
